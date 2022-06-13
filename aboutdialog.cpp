112e9534f805a6680dd585497ce027ce8e7c0a3c
Oleg Bobylev <38512160+OBobylev@users.noreply.github.com>
2022-06-13 05:16:20 +0300

    Update aboutdialog.cpp

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -8,6 +8,18 @@ aboutDialog::aboutDialog(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void aboutDialog::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QPen pen;
+    pen.setWidth(20);
+    pen.setColor(Qt::red);
+    painter.setPen(pen);
+    painter.setBrush(Qt::green);
+    painter.drawEllipse(50,50, 125, 125);
+    QWidget::paintEvent(event);
+}
+
 aboutDialog::~aboutDialog()
 {
     delete ui;
